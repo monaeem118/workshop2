42226866dfb57e0c2885b78abd65237617ce5fff
Muhammad Ovais Naeem <monaeem118@gmail.com>
2022-08-20 11:37:17 -0500

    second commit

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['586106695133.dkr.ecr.us-east-1.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ec2-3-130-63-215.us-east-2.compute.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
